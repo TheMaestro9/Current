8f24c92ce3e2adcef6db9ce1be4610c2a401c4e9
TheMaestro9 <walidmoussa995@gmail.com>
2018-12-23 12:15:51 +0200

    latest Edits

---
+++ Readme.txt
@@ -0,0 +1,1 @@
+VS code Or notepad++ 
---
+++ sendEmail.php
@@ -0,0 +1,21 @@
+<?php
+
+    // echo "php working" ;  
+    ob_start();
+    if(isset($_POST['submit'])) { 
+
+        // echo "inside the if" ; 
+        $name = $_POST['name'] ;
+        $mailFrom = $_POST['mail'] ; 
+        $message = $_POST['message'] ;  
+
+        $mailTo = "info@mdd-architects.com" ; 
+        $headers = "From: ".$mailFrom ;
+        $sideNote = "\n\ndon't reply to this email as it will be marked as for your potential client !"
+        $txt = "You hare received an email from ". $name."\n\n".$message.$sideNote;
+
+        mail($mailTo , "Mail From Your Website!" , $txt , $headers) ; 
+        header("Location: contact.html?mailsend=true");  
+    }
+
+?>
